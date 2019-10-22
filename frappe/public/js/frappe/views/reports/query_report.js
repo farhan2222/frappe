c49fa62d6d219d53da43ee074fcaa1009937aec0
Saif Ur Rehman <saifi0102@gmail.com>
2019-10-23 04:19:57 +0500

    fix: Do not force filters from url

--- frappe/public/js/frappe/views/reports/query_report.js
+++ frappe/public/js/frappe/views/reports/query_report.js
@@ -292,11 +292,6 @@ frappe.views.QueryReport = class QueryReport extends frappe.views.BaseList {
 		let filters = this.get_filter_values(true);
 		let query = frappe.utils.get_query_string(frappe.get_route_str());
 
-		if(query) {
-			let obj = frappe.utils.get_query_params(query);
-			filters = Object.assign(filters || {}, obj);
-		}
-
 		// only one refresh at a time
 		if (this.last_ajax) {
 			this.last_ajax.abort();
