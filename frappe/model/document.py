fdfb224553d181077bd24f34e612a6a88bc9bd33
farhan2222 <farhantayyab31@gmail.com>
2020-03-26 18:11:13 +0500

    Revert "fix: quotation save on delivery date"
    This reverts commit 0d2aba3050db07ee15d45ff978379c789f70109f.

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -443,8 +443,7 @@ class Document(BaseDocument):
 			d.docstatus = self.docstatus
 
 	def _validate(self):
-		if self.doctype != 'Quotation':
-			self._validate_mandatory()
+		self._validate_mandatory()
 		self._validate_selects()
 		self._validate_length()
 		self._extract_images_from_text_editor()
